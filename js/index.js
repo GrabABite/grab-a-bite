bbd4e0f7f48f5eee8ebd0e177040bd6b9b0e8e5e
imicaustria <ic.austria@ymail.com>
2020-03-29 21:18:39 -0400

    dropBtn to intro java, updated css

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let $doc = document.documentElement
+let $link = document.querySelector('header a')
+
+
+
+$link.addEventListener('click', event => { console.log('clicked') })
+
+
+$link.getAttribute('href')
+
+
+
