11ea4f31bef1aea4c7be5a03b632fa5ca812502a
imicaustria <ic.austria@ymail.com>
2020-04-02 12:46:19 -0400

    im stressedt

--- js/index.js
+++ js/index.js
@@ -1,12 +1,62 @@
 let $doc = document.documentElement
 let $link = document.querySelector('header a')
+let addComment = $('addComment'); 
+let commentText = $('comments');
+let commentList = $('commentList');
+
+
 
 
 
 $link.addEventListener('click', event => { console.log('clicked') })
-
-
 $link.getAttribute('href')
+
+
+
+
+
+
+
+
+
+
+
+// function $(element) {
+// 	return document.getElementById(element);
+// }
+
+// commentSubmit.addEventListener('click', function(e) {
+// 	e.preventDefault();
+  
+// 	let comment  = commentText.value.trim();
+// 	let newComm = document.createElement('li');
+  
+//   if (commentText.value.length > 1) {
+  
+// 	newComm.innerHTML = comment;
+// 	commentList.appendChild(newComm);
+
+//   } else {
+//     null
+//   }
+// }, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
